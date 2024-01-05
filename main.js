4463cbe53f69ad2891d45f6e401935dee23f630d
Semilore317 <semi.banks.317@gmail.com>
2024-01-05 23:20:49 +0100

    secondTry looking good, gotta workout

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-const burgerNavLinksA = document.querySelectorAll('.burger-nav-links a');
-const burgerNavLinks = document.querySelector('.burger-nav-links');
-const burgerIcon = document.querySelector('.burger-icon');
-
-
-
-
 function toggleMenu() {
-    burgerNavLinks.classList.toggle('display');
-    burgerIcon.classList.toggle('open');
-    console.log('cunt');
-}
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
 
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
